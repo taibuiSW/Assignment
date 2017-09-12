edd85686bdadc4bec74cb6ab80528ed9aa4fb197
Tai Bui <30861484+taibuiSW@users.noreply.github.com>
2017-09-12 09:38:50 +0700

    Revert "split ShuttleObject"
    This reverts commit 0232a85f3572e9e72c5e68a1994190c40dd177eb.

---
+++ Assets/Script/ExplodeEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ExplodeEffect : MonoBehaviour {
+    public float lifeTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime < 0) {
+            Destroy(gameObject);
+        }
+	}
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour {
 	private Animator animator;
     private Transform groundCheck;
     private bool isGrounded;
+	private Vector3 respawnPosition;
 	private LevelMgr levelMgr;
 
     // Use this for initialization
@@ -18,6 +19,7 @@ public class PlayerController : MonoBehaviour {
         rigidBody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
 		animator = GetComponent<Animator>();
+		respawnPosition = transform.position;
 		levelMgr = FindObjectOfType<LevelMgr> ();
         groundCheck = levelMgr.GetGroundCheck();
     }
@@ -49,6 +51,10 @@ public class PlayerController : MonoBehaviour {
 		if (other.tag == "Kill Zone") {
 			levelMgr.Respawn ();
 		}
+	}
+
+	public Vector3 GetRespawnPosition() {
+		return respawnPosition;
 	}
 
     private void OnEnable() {
