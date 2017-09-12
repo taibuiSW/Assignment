fec4ff4571b61ae66589cfb0e60fe2e0591b00fd
Tai Bui <30861484+taibuiSW@users.noreply.github.com>
2017-09-12 09:48:27 +0700

    commit

--- Assets/Script/ShuttleObject.cs
+++ Assets/Script/ShuttleObject.cs
@@ -5,64 +5,30 @@ public class ShuttleObject : MonoBehaviour {
     public float speed;
     public Transform endPoint;
     public bool flip;
-    public bool isEnemy;
-    [HideInInspector]
-    public int damage;
-    [HideInInspector]
-    public int score;
 
-    private Vector3 startPosition;
-    private Vector3 endPosition;
-    private Vector3 target;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private Vector3 targetPos;
     private bool canMove;
-    private LevelMgr levelMgr;
-    private Transform playerGroundCheck;
-
-    [CustomEditor(typeof(ShuttleObject))]
-    public class ShuttleEditor : Editor {
-        override public void OnInspectorGUI() {
-            base.OnInspectorGUI();
-            var shuttleObject = target as ShuttleObject;
-            GUI.enabled = shuttleObject.isEnemy;
-            shuttleObject.damage = EditorGUILayout.IntField("Damage", shuttleObject.damage);
-            shuttleObject.score = EditorGUILayout.IntField("Score", shuttleObject.score);
-        }
-    }
 
     // Use this for initialization
-    void Start () {
-        startPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        endPosition = endPoint.position;
-        target = endPosition;
-        if (isEnemy) {
-            levelMgr = FindObjectOfType<LevelMgr>();
-            playerGroundCheck = levelMgr.GetGroundCheck();
-        }
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start() {
+        startPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        endPos = endPoint.position;
+        targetPos = endPos;
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (canMove) {
-            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-            if (transform.position == startPosition) {
-                target = endPosition;
-                Flip();
-            } else if (transform.position == endPosition) {
-                target = startPosition;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+            if (transform.position == startPos) {
+                targetPos = endPos;
                 Flip();
             }
-            //transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-        }
-	}
-
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.tag == "Player" && isEnemy) {
-            if (playerGroundCheck.position.y > transform.position.y) {
-                other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 200f));
-                levelMgr.AddScore(score);
-                Destroy(gameObject);
-            } else {
-                levelMgr.DamagePlayer(damage);
+            else if (transform.position == endPos) {
+                targetPos = startPos;
+                Flip();
             }
         }
     }
